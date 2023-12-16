30675299889f834487c4971605013059d4a1787b
iayasin771@gmail.com <iayasin771@gmail.com>
2023-12-16 02:07:46 -0500

    Finish day 2

--- day2/part2.py
+++ day2/part2.py
@@ -1,19 +1,22 @@
 INPUT = "puzzle.txt"
 # INPUT = "easy.txt"
-total_right = 0
+total_sum = 0
 
 with open(INPUT) as file:
     for line in file:
         line = line.rstrip()
         game_num = int(line.split(":")[0].split()[1])
         sets = line.split(":")[1].strip().split(";")
-        marbles = {"red": float("inf"), "green": float("inf"), "blue": float("inf")}
+        marbles = {"red": 0, "green": 0, "blue": 0}
+
         for s in sets:
             for data in s.split(","):
                 count, color = data.strip().split(" ")
                 marbles[color] = max(marbles[color], int(count))
-        if marbles["red"] <= 12 and marbles["green"] <= 13 and marbles["blue"] <= 14:
-            total_right += game_num
-            print(game_num)
+        # print(marbles)
+        multiplication = 1
+        for num in marbles.values():
+            multiplication *= num
+        total_sum += multiplication
 
-print(total_right)
+print(total_sum)
